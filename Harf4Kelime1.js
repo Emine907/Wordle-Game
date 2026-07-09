4c10fc20c17c0fd499656078fdf2ccc6be5c3cf8
Emine907 <emineero61@gmail.com>
2026-07-09 16:41:17 +0300

    Update Harf4Kelime1.js

--- Harf4Kelime1.js
+++ Harf4Kelime1.js
@@ -15,7 +15,7 @@ function kontrolEt(hedef, girilen) {
     let sonuc = ["", "", "", ""];
 
     // 🟩 doğru harf + doğru yer
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
         if (girilenArr[i] === hedefArr[i]) {
             sonuc[i] = "green";
             hedefArr[i] = null;
